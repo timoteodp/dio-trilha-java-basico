915817be71904d61f915caf8e9932632d8cf2f21
Timóteo Dima Pereira <timoteo5@icloud.com>
2023-10-26 22:18:47 -0300

    Variáveis e Constantes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+       double salarioMinimo = 2.500;
+
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 = (short) numeroNormal;
+       System.out.println("O salário mínimo é "+ salarioMinimo);
+
+       int numero =4;
+       numero = 10;
+       System.out.println("O número agora é " + numero);
+
+       final double VALOR_DE_PI = 3.14;
+       System.out.println("Valor de Pi é " + VALOR_DE_PI);
+
+
     }
 }
